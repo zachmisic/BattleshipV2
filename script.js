7e7892759285f7eb296cb562b2e42dc87c31b02d
Alex Gisi <apgisi11@gmail.com>
2021-02-17 13:23:36 -0600

    Add big and small grid drawing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ main_row = document.getElementById("main-row");
 
 window.onload = function() {
 	display.init();
+	// TODO: initialize game object.
 };
 
 let display = {
@@ -20,6 +21,12 @@ let display = {
 	init: function() {
 		this.reset();
 		this.setEventListeners();
+
+		// Initialize grids.
+		this.initGrid(this.blue_big_grid);
+		this.initGrid(this.blue_small_grid);
+		this.initGrid(this.red_big_grid);
+		this.initGrid(this.red_small_grid);
 	},
 
 	setEventListeners: function() {
@@ -30,6 +37,8 @@ let display = {
 			this.hideStartMenu();
 			this.showBlueBigGrid();
 			this.showBlueSmallGrid();
+
+
 		});
 
 		this.reset_btn.addEventListener("click", () => {
@@ -91,11 +100,11 @@ let display = {
 	},
 
 	showRedBigGrid: function() {
-		this.red_big_grid.style.display = "block";
+		this.red_big_grid.style.display = "table";
 	},
 
 	showRedSmallGrid: function() {
-		this.red_small_grid.style.display = "block";
+		this.red_small_grid.style.display = "table";
 	},
 
 	hideRedSmallGrid: function () {
@@ -107,7 +116,7 @@ let display = {
 	},
 
 	showBlueBigGrid: function() {
-		this.blue_big_grid.style.display = "block";
+		this.blue_big_grid.style.display = "table";
 	},
 
 	hideBlueSmallGrid: function() {
@@ -115,7 +124,7 @@ let display = {
 	},
 
 	showBlueSmallGrid: function() {
-		this.blue_small_grid.style.display = "block";
+		this.blue_small_grid.style.display = "table";
 	},
 
 	hideAll: function() {
@@ -135,160 +144,31 @@ let display = {
 	},
 
 	/*
-		Function: init
-		Takes: NONE
-		Returns: a 10x10 table HTML element
-						function uses the .appendChild method to add 10 rows and 10 units per row to the table.
-		as the column elements are created, they are assigned a unique ID number "##" where the
-		first # is the row number and the secnd # is the column number. an eventListener is also
-		added to each column elements, that will detect 
-	*/
-	drawGrid: function() {
-		let ident = document.createElement('div');
-		let row = ""; // <tr> html element for table rows
-		let elem = ""; // grid cell
-		ident.id = "identifiers";
-		document.querySelector("body").appendChild(ident);
-		for(let i=0;i<10;i++)
-		{	
-			// creates identifiers for the rows
-			ident = document.createElement('div');
-			ident.style = "position: absolute; top: 60px; left: " + ((44*i)+505) + "px; font-size: 20px; text-alight: center; padding: 10px; width: 30px; height: 30px;";
-			ident.innerText = i+1
-			document.querySelector("#identifiers").appendChild(ident);
-			// creates identifiers for the columns
-			ident = document.createElement('div');
-			ident.style = "position: absolute; top: " + ((44*i)+105) + "px; left: 460px; font-size: 20px; text-alight: center; padding: 10px; width: 30px; height: 30 px;";
-			ident.innerText = String.fromCharCode(65+i);
-			document.querySelector("#identifiers").appendChild(ident);
-			// creates the row element for the table
-			row = grid.appendChild(document.createElement("tr"));
-			for(let j=0;j<10;j++)
-			{
-				elem = document.createElement("td");
-				elem.className = "grid-element";
-				elem.id = "" + i + j;
-				elem.addEventListener("click", (() => {	
-				if(display.gameStart) {
-					document.getElementById("check").innerText = i + " " + j;
-					document.getElementById(i + "" + j).style = "background-color: red;"; /*hitCheck*/
-					console.log(i + "" + j);
-					} 
-					else { 
-					/* place player current ship */
-					}
-				}));
-				row.appendChild(elem);
-			}
+	Initializes a grid given a node of a table element. Only called once per table, unless you want to
+	reset it.
+	 */
+	initGrid: function(table_ref) {
+		let headrow = document.createElement('tr');
+		for(let i=0; i < 10; i++) {
+			let box = document.createElement("th");
+			box.classList.add("grid-box");
+			box.classList.add("head");
+			headrow.appendChild(box);
 		}
-	},
-	
-	drawMiniMap: function() {
-		display.miniMap.id = "mini-map";
-		let ident = document.createElement('div');
-		let row = "";
-		let elem = "";
-		ident.id = "identifiers-mini";
-		document.querySelector("body").appendChild(ident);
-		for(let i=0;i<10;i++)
-		{	
-			// creates identifiers for the rows
-			ident = document.createElement('div');
-			ident.style = "position: absolute; top: 70px; left: " + ((13*i)+973) + "px; font-size: 15px; text-alight: center; padding: 10px; width: 30px; height: 30px;";
-			ident.innerText = i+1
-			document.querySelector("#identifiers").appendChild(ident);
-			// creates identifiers for the columns
-			ident = document.createElement('div');
-			ident.style = "position: absolute; top: " + ((14*i)+88) + "px; left: 955px; font-size: 13px; text-alight: center; padding: 10px; width: 30px; height: 30 px;";
-			ident.innerText = String.fromCharCode(65+i);
-			document.querySelector("#identifiers").appendChild(ident);
-			// creates the row element for the table
-			row = display.miniMap.appendChild(document.createElement("tr"));
-			for(let j=0;j<10;j++)
-			{
-				elem = document.createElement("td");
-				elem.className = "mini-map-elem";
-				elem.id = "" + i + j;
-				//elem.addEventListener("click", (() => {	
-				//document.getElementById("check").innerText = i + " " + j;
-				//document.getElementById(i + "" + j).style = "background-color: red;"; /*hitCheck*/
-				//console.log(i + "" + j);
-				row.appendChild(elem);
-			}
-		}
-	},
-	
-	/*
-		Takes: NONE
-		Returns: NONE
+		table_ref.appendChild(headrow);
 
-		function initializes the menu screen. It creates a new menu elements and appends them onto the body/background.
-		It also assignes them specific IDs so they will get styled by the css in index.html. After creation,
-		the elements are formatted in case of a griz size change.
-		
-		Creates Elements: "main-menu", "title", "start-button"
-	*/
-	drawMainMenu: function() {
-		let title = "";
-		let startBtn = "";
-		// main menu
-		mainMenu = document.querySelector('body').appendChild(document.createElement('div'));
-		mainMenu.id = "main-menu";
-			// mainMenu formatting
-		mainMenu.style.width = (grid.offsetWidth-2) + "";
-		mainMenu.style.height = (grid.offsetHeight-2) + "";
-		mainMenu.style.top = grid.offsetTop + "";
-		mainMenu.style.left = grid.offsetLeft + "";
-		// title text
-		title = document.querySelector('#main-menu').appendChild(document.createElement('div'));
-		title.id = "title";
-		title.innerText = "BATTLESHIP";
-			// title formatting
-		title.style.top = ((mainMenu.offsetHeight / 2) - (title.offsetHeight)) + "";
-		title.style.left = ((mainMenu.offsetWidth / 2) - (title.offsetWidth / 2)) + "";
-		// start button
-		startBtn = document.querySelector('#main-menu').appendChild(document.createElement('button'));
-		startBtn.id = "start-button";
-		startBtn.innerText = "Start!";
-			//start button formatting
-		startBtn.style.top = (title.offsetTop + 40) + "";
-		startBtn.style.left = (title.offsetLeft + (title.offsetLeft/2)/2) + "";
-		startBtn.addEventListener("click", () => { mainMenu.innerHTML = ""; display.drawSelMenu();});
-		
+		for(let i=0; i < 10; i++) {
+			let row = document.createElement('tr');
+			row.classList.add("grid-row");
+			for(let j=0; j < 10; j++) {
+				let box = document.createElement('td');
+				box.classList.add("grid-box");
+				row.appendChild(box);
+			}
+			table_ref.appendChild(row);
+		}
 	},
 
-	/*
-		Takes: NONE
-		Returns: NONE
-		
-		function initilaizes the selection menu and allows user to choose number of ships to play with. each selection
-		element has en event listener that listens for a click, and then calls a set ship # function.
-	*/
-	drawSelMenu: function() {
-		let title = document.querySelector('#main-menu').appendChild(document.createElement('div'));
-		let startBtn = "";
-		title.id = "title";
-		title.innerText = "Choose Number of Ships";
-			// title formatting
-		title.style.top = ((mainMenu.offsetHeight / 3) - (title.offsetHeight)) + "";
-		title.style.left = ((mainMenu.offsetWidth / 2) - (title.offsetWidth / 2)) + "";
-		// button formatting
-		/* 
-			you have to save the number i for each button so the event handler can select the specific id with the querySelector. 
-			if you don't assign a unique id, the eventListner only works for the last element created.
-		*/
-		for(let i=0;i<6;i++)
-		{
-			startBtn = document.querySelector('#main-menu').appendChild(document.createElement('div'));
-			startBtn.id = "btn" + i;
-			startBtn.style = "position: absolute; left: " + ((i*(grid.offsetWidth/6)+9)) + "px; top: " + (grid.offsetHeight/2) + "px; z-index: 1; width: 50px; height: 50px; border-style: solid; font-size: 50; text-align: center;";
-			startBtn.innerText = "" + (i+1);
-			startBtn.addEventListener("mouseover", () => { document.querySelector(("#btn" + i)).style.backgroundColor = "red"; }); 
-			startBtn.addEventListener("mouseout", () => { document.querySelector(("#btn" + i)).style.backgroundColor = ""; });
-			startBtn.addEventListener("click", () => { document.querySelector(("#btn" + i)).style.backgroundColor = "green"; /* setNumShips(i); */ mainMenu.innerHTML = ""; mainMenu.remove(); });
-			mainMenu.appendChild(startBtn);
-		}
-	},
 	/*
 		Takes: n => number of the largest ship
 		Returns: NONE
